dd3751aee371371c46fa1be6df71e69d4b21acb7
kiru21125 <kiru21125.it@rmkec.ac.in>
2024-07-16 19:12:47 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TicTacToe from './components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+   <div>
+
+     <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
